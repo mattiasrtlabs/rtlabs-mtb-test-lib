79bd9d121bfb31a26eae99913404d7be3f44e2f0
mattiasrtlabs <34244802+mattiasrtlabs@users.noreply.github.com>
2024-02-13 13:00:26 +0100

    Added test library binary

---
+++ libs/test_lib.h
@@ -0,0 +1,15 @@
+/*
+ * test_lib.h
+ *
+ *  Created on: 13 Feb 2024
+ *      Author: matti
+ */
+
+#ifndef TEST_LIB_H_
+#define TEST_LIB_H_
+
+void rtlabs_demo_lib(void);
+
+
+
+#endif /* TEST_LIB_H_ */
